508da0566bba683e1936fe3a7c5ddd8ac13e9421
Princess Nwanekwu <nwanekwup458@gmail.com>
2024-07-03 21:52:10 -0700

    quiz-screen demo questions

--- frontend/src/components/TakeQuiz.jsx
+++ frontend/src/components/TakeQuiz.jsx
@@ -1,21 +1,71 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./TakeQuiz.css";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+
+const questions = [
+  {
+    question: "I feel anxious most days",
+    answers: [5, 4, 3, 2, 1],
+  },
+  {
+    question: "I feel happy most days",
+    answers: [5, 4, 3, 2, 1],
+  },
+  {
+    question: "I feel motivated most days",
+    answers: [5, 4, 3, 2, 1],
+  },
+  {
+    question: "I feel calm most days",
+    answers: [5, 4, 3, 2, 1],
+  },
+  {
+    question: "I feel goofy most days",
+    answers: [5, 4, 3, 2, 1],
+  },
+];
 
 const TakeQuiz = () => {
-    
-    return (
-        <div className="quiz-container">
-            <h1>Music Personality Quiz</h1>
-            <p>Which of the following best describes your mood today?</p>
-            <div className="answer-options">
-                <button className="answer-button">Happy and Energized</button>
-                <button className="answer-button">Relaxed and Calm</button>
-                <button className="answer-button">Anxious and Overwhelmed</button>
-            </div>
-            <button className="next-button">Next</button>
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
 
+  const handleNextClick = () => {
+    setCurrentQuestion(currentQuestion + 1);
+  };
+
+  const handleAnswerClick = (answer) => {
+    setAnswers([...answers, answer]);
+  };
+
+  return (
+    <div className="quiz-container">
+      <header className="header">
+        <h1>Music Personality Quiz</h1>
+      </header>
+      {questions[currentQuestion] && (
+        <div className="option-question">
+          <h3>{questions[currentQuestion].question}</h3>
         </div>
-    )
-}
+      )}
+      {questions[currentQuestion] && (
+        <div className="answer-options">
+          {questions[currentQuestion].answers.map((answer) => (
+            <button
+              key={answer}
+              className="answer-button"
+              onClick={() => handleAnswerClick(answer)}
+            >
+              {answer}
+            </button>
+          ))}
+        </div>
+      )}
+      {currentQuestion < questions.length - 1 && (
+        <button className="next-button" onClick={handleNextClick}>
+          Next
+        </button>
+      )}
+    </div>
+  );
+};
 export default TakeQuiz;
