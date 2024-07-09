458a71c6157ac4454c480ca4772b9ca1fe854534
Princess Nwanekwu <nwanekwup458@gmail.com>
2024-07-09 07:03:57 -0700

    feature to save to database

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import './Header.css' 
+import React from "react";
+import "./Header.css";
 
 function Header() {
-    return (
-        <header className="header">
-            <h1>RhythME</h1>
-        </header>
-    )
+  return (
+    <header className="header">
+      <h1 className="logo">RhythME</h1>
+      <nav className="nav">
+        <ul>
+          <li>
+            <a href="#" className="nav-link">
+              Home
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
