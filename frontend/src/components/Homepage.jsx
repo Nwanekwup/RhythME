afd57309c31f25a1a7cba7ac0b9d806a125a5291
Princess Nwanekwu <nwanekwup458@gmail.com>
2024-07-09 23:25:31 -0700

    log out button
    - when log out is clicked, user will be taken back to the welcome page for log in or sign in.
    -minor css styling

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -4,10 +4,39 @@ import Header from "./Header"
 
 
 function Homepage() {
+<<<<<<< Updated upstream
+=======
+  const { userId } = useParams();
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    navigate('/');
+  }
+>>>>>>> Stashed changes
   return (
     <div>
       <Header />
+<<<<<<< Updated upstream
       <p>This is the homepage.</p>
+=======
+      <div className="sidebar-container">
+        <div className="profile-icon-container">
+          <img src="profile-icon.png" alt="Profile Icon" />
+          <button className="log-out-btn" onClick={handleLogOut}>
+            Log Out
+          </button>
+        </div>
+      </div>
+      <div className="main-content-container">
+        <h1>Welcome to RhythME!</h1>
+        <button
+          className="take-quiz-btn"
+          onClick={() => navigate(`/take-quiz/${userId}`)}
+        >
+          <i className="fas fa-music">Find your match</i>
+        </button>
+      </div>
+>>>>>>> Stashed changes
     </div>
   )
 
--- frontend/src/components/TakeQuiz.jsx
+++ frontend/src/components/TakeQuiz.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
 import "./TakeQuiz.css";
+<<<<<<< Updated upstream
 import { useNavigate } from 'react-router-dom';
+=======
+import { useNavigate, useParams } from "react-router-dom";
+
+
+const backendAddress = import.meta.env.VITE_BACKEND_ADDRESS;
+const questions = [
+  {
+    question: "I feel anxious most days",
+    answers: [5, 4, 3, 2, 1],
+  },
+  {
+    question: "I feel happy most days",
+    answers: [5, 4, 3, 2, 1],
+  },
+  {
+    question: "I feel motivated most days",
+    answers: [5, 4, 3, 2, 1],
+  },
+  {
+    question: "I feel calm most days",
+    answers: [5, 4, 3, 2, 1],
+  },
+  {
+    question: "I feel goofy most days",
+    answers: [5, 4, 3, 2, 1],
+  },
+];
+>>>>>>> Stashed changes
 
 const TakeQuiz = () => {
     
@@ -15,6 +44,52 @@ const TakeQuiz = () => {
             </div>
             <button className="next-button">Next</button>
 
+<<<<<<< Updated upstream
+=======
+  const handleNextClick = () => {
+    setCurrentQuestion(currentQuestion + 1);
+  };
+
+  const handleAnswerClick = (answer) => {
+    const updatedAnswers = [...answers];
+    updatedAnswers[currentQuestion] = answer;
+    setAnswers(updatedAnswers);
+    setQuestion([...answeredQuestions, questions[currentQuestion].question]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const answersData = {
+      userId: userId,
+      answers: answers,
+      questions: answeredQuestions,
+    };
+    try {
+      const response = await fetch(`${backendAddress}/submit-answers`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(answersData),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        navigate(`/home/${data.userId}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="quiz-container">
+      <header className="header">
+        <h1>Music Personality Quiz</h1>
+      </header>
+      {questions[currentQuestion] && (
+        <div className="option-question">
+          <h3>{questions[currentQuestion].question}</h3>
+>>>>>>> Stashed changes
         </div>
     )
 }
